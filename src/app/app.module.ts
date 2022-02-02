55667f5bb9ffb2103a0035180c2cf5d89ae19003
Kalki Nethaji <kalki@skaas.guru>
2022-02-02 12:41:13 +0530

    Add Cart, Product and ProductList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ProductListComponent,
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
